0beec1394bef3d98446f465e14a47d85af778f8b
Jan <krimdomu@googlemail.com>
2010-12-21 01:51:11 +0100

    init

---
+++ lib/Rex/Bundle.pm
@@ -0,0 +1,14 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+
+package Rex::Bundle;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.1';
+
+1;
